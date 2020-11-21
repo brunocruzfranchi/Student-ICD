65ea073a4aed9caa99d55786b69f1f7d19a9be05
Yen lo Wang <ivanbumaschny@gmail.com>
2020-11-21 19:59:50 -0300

    Start Position

--- student-merge.R
+++ student-merge.R
@@ -2,13 +2,18 @@
 
 ##importe de los datos
 
-setwd("E:/Bruno/Favaloro/4 Año/2do Cuatrimestre/ICD/ProyectoFinal")
+setwd("E:/Documentos/ING_BIOMEDICA/Cuarto Año/Segundo Cuatrimestre/Introducción a la Ciencia de Datos/Student-ICD")
 
-d1=read.table("student-mat.csv",sep=",",header=TRUE)
-d2=read.table("student-por.csv",sep=",",header=TRUE)
+matematica = read.table("student-mat.csv",sep=",",header=TRUE)
+portugues = read.table("student-por.csv",sep=",",header=TRUE)
 
-d3=merge(d1,d2,by = c("school","sex","age","address","famsize","Pstatus","Medu",
-                      "Fedu","Mjob","Fjob","reason","nursery","internet"))
+dset = merge(matematica, portugues, by = c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
+# print(nrow(d4)) # 382 students
 
-print(nrow(d3)) # 382 students
+names(dset)[names(dset) == 'G1.x'] <- 'G1.Matematica'
+names(dset)[names(dset) == 'G2.x'] <- 'G2.Matematica'
+names(dset)[names(dset) == 'G3.x'] <- 'G3.Matematica'
+names(dset)[names(dset) == 'G1.y'] <- 'G1.Portugues'
+names(dset)[names(dset) == 'G2.y'] <- 'G2.Portugues'
+names(dset)[names(dset) == 'G3.y'] <- 'G3.Portugues'
 
