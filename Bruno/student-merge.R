584c8b6b7ddcbefe9a7297df057def4d00af52bb
Bruno Cruz Franchi <brunocruzfranchi@gmail.com>
2020-11-28 20:17:03 -0300

    PreparandoDatasets

--- Bruno/student-merge.R
+++ Bruno/student-merge.R
@@ -1,5 +1,3 @@
-
-
 ##importe de los datos
 
 library(ggplot2)
@@ -10,7 +8,6 @@ library(factoextra)
 
 setwd("E:/Bruno/Favaloro/4 Año/2do Cuatrimestre/ICD/ProyectoFinal/Student-ICD")
 
-d1=read.table("student-mat.csv",sep=",",header=TRUE)
 d2=read.table("student-por.csv",sep=",",header=TRUE)
 
 d3=merge(d1,d2,by = c("school","sex","age","address","famsize","Pstatus","Medu",
@@ -18,7 +15,30 @@ d3=merge(d1,d2,by = c("school","sex","age","address","famsize","Pstatus","Medu",
 
 print(nrow(d3)) # 382 students
 
-######################### Primera Regresion Logistica ##########################
+########################## Arreglo de los datasets #############################
+
+Math=read.table("student-mat.csv",sep=",",header=TRUE)
+
+Math$school <- factor(Math$school)
+Math$sex <- factor(Math$sex)
+Math$address <- factor(Math$address)
+Math$famsize <- factor(Math$famsize)
+Math$Pstatus <- factor(Math$Pstatus)
+Math$Mjob <- factor(Math$Mjob)
+Math$Fjob <- factor(Math$Fjob)
+Math$reason <- factor(Math$reason)
+Math$guardian <- factor(Math$guardian)
+Math$schoolsup <- factor(Math$schoolsup)
+Math$famsup <- factor(Math$famsup)
+Math$paid <- factor(Math$Pstatus)
+Math$Mjob <- factor(Math$Mjob)
+Math$Fjob <- factor(Math$Fjob)
+Math$reason <- factor(Math$reason)
+Math$guardian <- factor(Math$guardian)
+
+
+
+######################### Primera Regresion ##########################
 
 #Simple Regresion Linear para identificar si hay algun tipo de variables
 #significativas
@@ -37,6 +57,22 @@ abline(0,1)
 
 #Famrel+Absences+G1+G2
 
-
 lm.linear <- lm((G3.x+G3.y)~.,data=d3)
 summary(lm.linear)
+
+######################### Primera Regresion  ##########################
+
+## Separacion de datos en training y testing
+library(caTools)
+set.seed(123)   
+sample = sample.split(d1,SplitRatio = 0.70) 
+train =subset(d1,sample == TRUE)
+test=subset(d1, sample == FALSE)
+
+lm.linear <- lm(G3~., data = train)
+
+summary(lm.linear)
+
+y_hat <- predict(lm.linear, newdata = test)
+
+ecm <- mean((test$G3-round(y_hat)))
